be328b1a1f3ad2d954744ff8c9543bf85af0c9e8
Vladlen Korablev <korablev.vlm@gmail.com>
2025-06-18 10:08:32 +0300

    refactor(MS-calculator): refactor swagger config

--- MS-calculator/src/main/java/ru/creditservices/calculator/MsCalculatorApplication.java
+++ MS-calculator/src/main/java/ru/creditservices/calculator/MsCalculatorApplication.java
@@ -1,17 +1,8 @@
 package ru.creditservices.calculator;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(
-        info = @Info(
-                title = "Credit Services Calculator API",
-                version = "0.8.1",
-                description = "API for calculating loan offers and credit scoring"
-        )
-)
 @SpringBootApplication
 public class MsCalculatorApplication {
 
